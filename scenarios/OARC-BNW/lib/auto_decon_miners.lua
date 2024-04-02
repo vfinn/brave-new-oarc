c973cf8de02b210c5264a04bf41dd3597a7044b2
JustGoFly <vfinn@yahoo.com>
2024-04-01 21:41:54 -0400

    Rectangle around decon'd miner

--- scenarios/OARC-BNW/lib/auto_decon_miners.lua
+++ scenarios/OARC-BNW/lib/auto_decon_miners.lua
@@ -13,6 +13,10 @@ function OarcAutoDeconOnTick()
         for i,miner in pairs(global.oarc_decon_miners) do
             if (miner and miner.valid and miner.mining_target == nil) then
                 miner.order_deconstruction(miner.force)
+                if miner.last_user then 
+                    rendering.draw_rectangle{color={1,0.1,0.1,1}, left_top=miner.selection_box.left_top, right_bottom=miner.selection_box.right_bottom, surface="oarc", time_to_live=60*15, miner.last_user.force, draw_on_ground=false}
+--                    game.players[miner.last_user.index].print("Removing miner: " .. miner.gps_tag)
+                end
             end
             table.remove(global.oarc_decon_miners, i)
         end
